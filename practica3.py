d8eb1eb3ab96e85a3c35d01b8221e76556717b10
f <felipeperez2710@gamil.com>
2017-10-19 22:58:46 +0200

    falta mask

--- practica3.py
+++ practica3.py
@@ -3,10 +3,7 @@ import cv2
 from scipy import fftpack
 
 
-img = cv2.imread("Lenna.png",0) #gray
-width,height = img.shape
-width = int (width/8)
-height = int(height/8)
+
 
 def get_2D_dct(img):
     """ Get 2D Cosine Transform of Image
@@ -39,27 +36,59 @@ def create_mask(porcentaje):
 
 
 
+if __name__=="__main__":
 
-eo = create_mask(50)
-print(eo)
-'''
-mask= np.zeros((8,8))
-mask[0,0]=1
-img_reducida = np.zeros(img.shape,dtype='uint8')
+    img = cv2.imread("Lenna.png", 0)  # gray
+    width, height = img.shape
+    width = int(width / 8)
+    height = int(height / 8)
 
-for i in range(0,width):
-    for j in range(0,height):
-        block = img[8*i:8*(i+1),(j*8):8*(j+1)]
-        block_dct = get_2D_dct(block)
-        block_dct = np.multiply(mask,block_dct) #reduciendo
-        img_reducida[8*i:8*(i+1),(j*8):8*(j+1)] = get_2d_idct(block_dct)
+    mask= np.zeros((8,8))
+    mask[0,0]=1
 
-cv2.imshow("imagen original",img)
-cv2.imshow("imagen reducida",img_reducida)
-cv2.waitKey()
+    img_reducida = np.zeros(img.shape,dtype='uint8')
+
+    for i in range(0,width):
+        for j in range(0,height):
+            block = img[8*i:8*(i+1),(j*8):8*(j+1)]
+            block_dct = get_2D_dct(block)
+            block_dct = np.multiply(mask,block_dct) #reduciendo
+            img_reducida[8*i:8*(i+1),(j*8):8*(j+1)] = get_2d_idct(block_dct)
+
+    cv2.imshow("imagen original",img)
+    cv2.imshow("imagen reducida",img_reducida)
+    cv2.waitKey()
 
 
-'''
+    ############### Reduccion JPG ########################
+    luminancia = np.array([[16,11,10,16,24,40,51,61],
+                          [12,12,14,19,26,58,60,55],
+                          [14,13,16,24,40,57,69,56],
+                          [14,17,22,29,51,87,80,62],
+                          [18,22,37,56,68,109,103,77],
+                          [24,36,55,64,81,104,113,92],
+                          [49,64,78,87,103,121,210,101],
+                          [72,92,95,98,112,100,103,99]])
+    imgJPG=img
+    imgJPG=imgJPG-255*np.ones(imgJPG.shape)
+    for i in range(0,width):
+        for j in range(0,height):
+            block = imgJPG[8*i:8*(i+1),(j*8):8*(j+1)]
+
+            block_dct = get_2D_dct(block)
+            #se divide por el factor de calidad
+            block_dct=np.divide(block_dct,luminancia)
+            #se redondea
+            block_dct=np.array(block_dct,dtype='int8')
+
+            block_dct = block_dct+255*np.ones(block_dct.shape)
+            img_reducida[8 * i:8 * (i + 1), (j * 8):8 * (j + 1)] = get_2d_idct(block_dct)
+
+    cv2.imshow("imagen original", img)
+    cv2.imshow("imagen 'jpg'", img_reducida)
+    cv2.waitKey()
+
+
 
 
 
