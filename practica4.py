6ab1e3a9f8ea6c20a7299163d4d44d282c2fc67d
f <felipeperez2710@gamil.com>
2017-10-19 22:33:28 +0200

    no funciona

--- practica4.py
+++ practica4.py
@@ -1,21 +1,19 @@
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
+from numba import jit
 
-
-
+@jit
 def calcula_SSD(xr,yr,xl,yl,f,h,block):
-    sum = 0
+
+    inc = int(block[0] / 2)
+    block_f = f[xr - inc:xr + inc+1, yr - inc:yr + inc+1]
+    block_h = h[xl, yl] * np.ones(block)
 
 
-    for i in range(-int(block[0]/2),+int(block[0]/2)+1):
-        for j in range(-int(block[1]/2),+int(block[1]/2)+1):
-            sum = sum + (f[xr+i,yr+j]-h[xl,yl])**2
-    sum = 1+sum
-    SSD = 1/sum
-    '''
-    optimizar con los np.sum 
-    '''
+    SSD = 1/(1+np.sum((block_f-block_h)**2))
+
+
     return SSD
 
 
@@ -25,20 +23,20 @@ block=(11,11)
 max_dispar = 50
 
 if __name__=="__main__":
-    imgR = cv2.imread("b1.jpg",0)
-    imgL = cv2.imread("b2.jpg",0)
+    imgR = cv2.imread("a2.jpg",0)
+    imgL = cv2.imread("a1.jpg",0)
     disparidad = np.zeros(imgR.shape,dtype='uint8')
 
     for x in range(int(block[0]/2),imgR.shape[0]-int(block[0]/2)):
-        for y in range(int(block[1]/2),imgR.shape[1]-int(block[1]/2)):
+        for y in range(int(block[1]/2),imgR.shape[1]-int(block[1]/2)-1):
             #Para el pixel "x" e "y"
 
             flag = False
             for it in range(0,max_dispar):
-                if x-it<0: #nos hemos pasado de la imagen de la izquierda
+                if y-it<=5: #nos hemos pasado de la imagen de la izquierda
                     break
                 else:
-                    SSD = calcula_SSD(xr=x-it,yr=y,
+                    SSD = calcula_SSD(xr=x,yr=y-it,
                                           xl=x,yl=y,
                                           f=imgR,h=imgL,
                                           block=block)
@@ -57,7 +55,8 @@ if __name__=="__main__":
 
 
 
-            print("Pos [", x, ",", y, "]"," disparidad: ",disparidad[x,y])
+            print("Pos [", x, ",", y, "]"," disparidad: ",disparidad[x,y],
+                  " SSD: ",min)
 
 
     max_dispar = np.amax(disparidad)
@@ -65,13 +64,7 @@ if __name__=="__main__":
 
     cv2.imwrite("disparidad.png", disparidad)
 
-    plt.subplot(131)
-    plt.imshow(imgR,cmap='gray')
-    plt.title("Right image")
-    plt.subplot(132)
-    plt.imshow(imgL,cmap='gray')
-    plt.title("Left image")
-    plt.subplot(133)
+
     plt.imshow(disparidad,cmap='gray')
     plt.title("Disparidad")
 
