c3625d8c082d020acf0562c22ee0d08c627049fe
f <felipeperez2710@gamil.com>
2017-10-25 21:09:12 +0200

    Preguntar si Sobel en frecuencia esta bien

--- practica2.py
+++ practica2.py
@@ -116,18 +116,28 @@ def sobelmask(tam,orientation):
 
     return mask
 
+def sobel_dom_imagen(img,orientation):
+    sobel = np.matrix('-1,0,1;-2,0,2;-1,0,1')
+    result= np.zeros(img.shape,dtype='uint8')
+
+    if orientation != 'x':  # orientation = 'y'
+        sobel = sobel.getT()
+
+    for i in range(1,img.shape[0]-1):
+        for j in range(1,img.shape[1]-1):
+
+            block_img = img[i-1:i+2,j-1:j+2]
+            result[i,j]=abs(0.25*np.sum(np.multiply(sobel,block_img)))
+
+    return result
+
 if __name__ == "__main__":
 
     image = imGenerator((100,150),"cos",1,1)
     freq = imfft(image)
     image_freq = np.array(abs(255*(freq).real),dtype='uint8')
 
-    '''
-    cv2.imshow("imagen generada",image)
-    cv2.imshow("imagen freq",image_freq)
-    cv2.waitKey()
-    immagphase(freq)
-    '''
+
     lenna = cv2.imread('Lenna.png', flags=0)
     lenna_fft = imfft(lenna)
 
@@ -182,7 +192,7 @@ if __name__ == "__main__":
 
     # -----------------Sobel--------------------------- #
 
-
+    ## Dominio de la frecuencia
     # Sobel en eje x
     mask_x = sobelmask(tam=lenna_fft.shape, orientation='x')
     result_fft_x = np.multiply(lenna_fft,mask_x)
@@ -193,18 +203,20 @@ if __name__ == "__main__":
     result_fft_y = np.multiply(lenna_fft, mask_y)
     result_y = np.array(abs(np.fft.ifft2(result_fft_y).real), dtype='uint8')
 
-    # Sobel en ambos ejes
-    result_fft_xy = np.multiply(result_fft_x, mask_y)
-    result_xy = np.array(abs(np.fft.ifft2(result_fft_xy).real), dtype='uint8')
 
 
-    _,axarr = plt.subplots(2,2)
-    axarr[0,0].imshow(lenna,cmap='gray')
-    axarr[0, 0].set_title('imagen original')
-    axarr[0, 1].imshow(result_x,cmap='gray')
-    axarr[0, 1].set_title('Sobel en direccion x')
-    axarr[1, 0].imshow(result_y,cmap='gray')
-    axarr[1, 0].set_title('Sobel en direccion y')
-    axarr[1, 1].imshow(result_xy,cmap='gray')
-    axarr[1, 1].set_title('Sobel en direccion x e y')
+
+    ## Dominio de la imagen
+    imgSobelx = sobel_dom_imagen(img=lenna,orientation='x')
+    imgSobely = sobel_dom_imagen(img=lenna,orientation='y')
+
+    _, axarr = plt.subplots(2, 2)
+    axarr[0, 0].imshow(result_x, cmap='gray')
+    axarr[0, 0].set_title('Sobel_x(Frecuencia)')
+    axarr[0, 1].imshow(imgSobelx, cmap='gray')
+    axarr[0, 1].set_title('Sobel_x(Imagen)')
+    axarr[1, 0].imshow(result_y, cmap='gray')
+    axarr[1, 0].set_title('Sobel_y(Frecuencia)')
+    axarr[1, 1].imshow(imgSobely, cmap='gray')
+    axarr[1, 1].set_title('Sobel_y(Imagen)')
     plt.show()
